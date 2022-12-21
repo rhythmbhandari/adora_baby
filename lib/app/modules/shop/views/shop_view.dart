0cad99773dc36cb962339eb2a61593d6778ef0af
Rhythm Bhandari <rhythmbhandari@outlook.com>
2022-12-21 10:46:37 +0545

    Shop view updated

--- lib/app/modules/shop/views/shop_view.dart
+++ lib/app/modules/shop/views/shop_view.dart
@@ -5,12 +5,14 @@ import 'package:adora_baby/app/widgets/all_brands.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:get/get.dart';
 import 'package:shimmer/shimmer.dart';
 
 import '../../../widgets/all_products.dart';
+import '../../../widgets/gradient_icon.dart';
 import '../../../widgets/hot_sales.dart';
 import '../../../config/app_colors.dart';
 import '../../../enums/progress_status.dart';
@@ -488,88 +490,323 @@ class OldShopViewBody extends StatelessWidget {
 Widget shimmerHomePage() {
   return Column(
     children: [
-      Container(
-        margin: const EdgeInsets.only(top: 20, bottom: 15, left: 18, right: 18),
-        decoration: BoxDecoration(
-            color: Colors.white.withOpacity(0.3),
-            borderRadius: BorderRadius.circular(20)),
-        child: Column(
+      ShimmerHotSales(),
+      SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
           children: [
-            const SizedBox(
-              height: 20,
+            for (int i = 0; i < 4; i++)
+              Container(
+                padding: const EdgeInsets.only(top: 10),
+                height: 286,
+                margin: const EdgeInsets.only(right: 0, bottom: 10, left: 19),
+                width: Get.width * 0.35,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(
+                        color: const Color.fromRGBO(192, 144, 254, 0.25)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 1,
+                        blurRadius: 2,
+                        offset: Offset(0, 2), // changes position of shadow
+                      ),
+                    ],
+                    borderRadius: BorderRadius.circular(15)),
+                child: Column(
+                  children: [
+                    Stack(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(top: 12, bottom: 8),
+                          child: Center(
+                            child: Image.network(
+                              "snapshot.data![0][index].productImages[0].name",
+                              height: Get.height * 0.16,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.only(
+                              top: 2, bottom: 2, left: 6, right: 6),
+                        )
+                      ],
+                    ),
+                    Container(
+                      padding: const EdgeInsets.only(left: 13, right: 13),
+                      width: Get.width,
+                      decoration: const BoxDecoration(
+                          color: Color.fromRGBO(243, 234, 249, 1),
+                          borderRadius: BorderRadius.only(
+                              bottomRight: Radius.circular(15),
+                              bottomLeft: Radius.circular(15))),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            "test",
+                            maxLines: 1,
+                            style: kThemeData.textTheme.labelSmall?.copyWith(
+                                color: AppColors.secondary700, fontSize: 12),
+                          ),
+                          SizedBox(
+                            height: 4,
+                          ),
+                          Text(
+                            "Test",
+                            maxLines: 2,
+                            style: kThemeData.textTheme.bodyMedium?.copyWith(
+                                color: AppColors.primary700, fontSize: 14),
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          RatingBar.builder(
+                            initialRating: 4,
+                            ignoreGestures: true,
+                            itemSize: 12,
+                            direction: Axis.horizontal,
+                            allowHalfRating: true,
+                            glow: false,
+                            itemCount: 5,
+                            itemPadding: EdgeInsets.symmetric(horizontal: 0.0),
+                            itemBuilder: (context, _) => GradientIcon(
+                              Icons.star,
+                              10.0,
+                              LinearGradient(
+                                colors: <Color>[
+                                  Color.fromRGBO(127, 0, 255, 1),
+                                  Color.fromRGBO(255, 0, 255, 1)
+                                ],
+                                begin: Alignment.topLeft,
+                                end: Alignment.bottomRight,
+                              ),
+                            ),
+                            onRatingUpdate: (rating) {
+                              print(rating);
+                            },
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                "Rs. ",
+                                maxLines: 2,
+                                style: kThemeData.textTheme.bodyMedium
+                                    ?.copyWith(
+                                        color: DarkTheme.lightActive,
+                                        decoration: TextDecoration.lineThrough),
+                              ),
+                              Text(
+                                "Rs. ",
+                                maxLines: 2,
+                                style: kThemeData.textTheme.bodyMedium
+                                    ?.copyWith(color: DarkTheme.normal),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+          ],
+        ),
+      ),
+      Container(
+        height: Get.height * 0.1,
+      )
+    ],
+  );
+}
+
+class ShimmerHotSales extends StatelessWidget {
+  const ShimmerHotSales({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 20, bottom: 15, left: 18, right: 18),
+      decoration: BoxDecoration(
+          color: Colors.white.withOpacity(0.3),
+          borderRadius: BorderRadius.circular(20)),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 20,
+          ),
+          Center(
+            child: Container(
+              height: 21,
+              width: 100,
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(40)),
             ),
-            Center(
-              child: Container(
+          ),
+          Center(
+            child: Container(
+              height: 21,
+              width: 100,
+              decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(40)),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 18),
+            child: GridView.count(
+              childAspectRatio: 0.6,
+              physics: NeverScrollableScrollPhysics(),
+              shrinkWrap: true,
+              crossAxisCount: 2,
+              children: List.generate(
+                4,
+                (index) => Container(
+                  padding: const EdgeInsets.only(top: 10),
+                  margin: EdgeInsets.only(right: 18, bottom: 20),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.8),
+                      border: Border.all(
+                          color: const Color.fromRGBO(192, 144, 254, 0.25)),
+                      borderRadius: BorderRadius.circular(15)),
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child: Container(
+              height: 21,
+              width: 100,
+              decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(40)),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                alignment: Alignment.bottomRight,
                 height: 21,
                 width: 100,
                 decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.circular(40)),
               ),
-            ),
-            Center(
-              child: Container(
-                height: 21,
-                width: 100,
-                decoration:
-                    BoxDecoration(borderRadius: BorderRadius.circular(40)),
+              SizedBox(
+                width: 20,
               ),
+            ],
+          ),
+          Center(
+            child: Container(
+              height: 21,
+              width: 100,
+              decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(40)),
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 18),
-              child: GridView.count(
-                childAspectRatio: 0.6,
-                physics: NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                crossAxisCount: 2,
-                children: List.generate(
-                  4,
-                  (index) => Container(
-                    padding: const EdgeInsets.only(top: 10),
-                    margin: EdgeInsets.only(right: 18, bottom: 20),
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                        color: Colors.white.withOpacity(0.8),
-                        border: Border.all(
-                            color: const Color.fromRGBO(192, 144, 254, 0.25)),
-                        borderRadius: BorderRadius.circular(15)),
-                  ),
-                ),
-              ),
-            ),
-            Center(
-              child: Container(
-                height: 21,
-                width: 100,
-                decoration:
-                BoxDecoration(borderRadius: BorderRadius.circular(40)),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Container(
-                  alignment: Alignment.bottomRight,
-                  height: 21,
-                  width: 100,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(40)),
-                ),
-                SizedBox(width: 20,),
-              ],
-            ),
-            Center(
-              child: Container(
-                height: 21,
-                width: 100,
-                decoration:
-                BoxDecoration(borderRadius: BorderRadius.circular(40)),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
-    ],
-  );
+    );
+  }
+}
+
+class ShimmerRecentlyViewed extends StatelessWidget {
+  const ShimmerRecentlyViewed({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 20, bottom: 15, left: 18, right: 18),
+      decoration: BoxDecoration(
+          color: Colors.white.withOpacity(0.3),
+          borderRadius: BorderRadius.circular(20)),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 20,
+          ),
+          Center(
+            child: Container(
+              height: 21,
+              width: 100,
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(40)),
+            ),
+          ),
+          Center(
+            child: Container(
+              height: 21,
+              width: 100,
+              decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(40)),
+            ),
+          ),
+          Row(
+            children: [
+              Container(
+                padding: const EdgeInsets.only(top: 10),
+                margin: EdgeInsets.only(right: 18, bottom: 20),
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.8),
+                    border: Border.all(
+                        color: const Color.fromRGBO(192, 144, 254, 0.25)),
+                    borderRadius: BorderRadius.circular(15)),
+              )
+            ],
+          ),
+          Center(
+            child: Container(
+              height: 21,
+              width: 100,
+              decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(40)),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Container(
+                alignment: Alignment.bottomRight,
+                height: 21,
+                width: 100,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(40)),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+            ],
+          ),
+          Center(
+            child: Container(
+              height: 21,
+              width: 100,
+              decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(40)),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
