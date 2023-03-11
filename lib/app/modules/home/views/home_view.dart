836c076ef31983fc9da9983826e28588ccbcf1b5
Rhythm Bhandari <rhythmbhandari@outlook.com>
2023-03-11 23:34:20 +0545

    Updated side bar

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 import 'package:get/get.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../../../main.dart';
 import '../../../config/app_colors.dart';
@@ -28,8 +31,9 @@ class HomeView extends GetView<HomeController> {
         return false;
       },
       child: Scaffold(
-        bottomSheet: BottomNavBar(scaffoldKey: _scaffoldKey,),
-
+        bottomSheet: BottomNavBar(
+          scaffoldKey: _scaffoldKey,
+        ),
         key: _scaffoldKey,
         drawerScrimColor: Colors.transparent,
         endDrawer: Container(
@@ -37,39 +41,312 @@ class HomeView extends GetView<HomeController> {
           child: Drawer(
             elevation: 0,
             backgroundColor: LightTheme.lightActive,
-            child: ListView(
-              // Important: Remove any padding from the ListView.
-              padding: EdgeInsets.symmetric(
-                  vertical: Get.height * 0.08, horizontal: 10),
-              children: [
-                ListTile(
-                  title: Text(
-                    'Logout',
-                    style: kThemeData.textTheme.bodyLarge
-                        ?.copyWith(color: DarkTheme.normal, fontSize: 18),
+            child: SafeArea(
+              child: Column(
+                // Important: Remove any padding from the ListView.
+
+                children: [
+                  Expanded(
+                    child: ListView(
+                      padding: EdgeInsets.symmetric(
+                          vertical: Get.height * 0.00, horizontal: 10),
+                      children: [
+                        ListTile(
+                          selectedTileColor: Colors.transparent,
+                          contentPadding: EdgeInsets.symmetric(
+                            horizontal: 24,
+                          ),
+                          title: Text(
+                            'Menu',
+                            style: kThemeData.textTheme.titleMedium?.copyWith(
+                              color: DarkTheme.normal,
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          onTap: () {
+                            _scaffoldKey.currentState!.closeEndDrawer();
+                          },
+                          trailing: SvgPicture.asset(
+                            "assets/images/close-circle.svg",
+                            height: 28,
+                            color: DarkTheme.normal,
+                          ),
+                        ),
+                        SizedBox(
+                          height: Get.height * 0.04,
+                        ),
+                        ListTile(
+                          selectedTileColor: Colors.transparent,
+                          contentPadding: EdgeInsets.symmetric(
+                            horizontal: 20,
+                          ),
+                          minLeadingWidth: 0,
+                          title: Text(
+                            'Change Password',
+                            style: kThemeData.textTheme.bodyLarge?.copyWith(
+                              color: DarkTheme.normal,
+                              fontSize: 18,
+                            ),
+                          ),
+                          onTap: () {
+                            _scaffoldKey.currentState!.closeEndDrawer();
+                          },
+                          leading: SvgPicture.asset(
+                            "assets/images/eye.svg",
+                            height: 26,
+                            // width: 28,
+                            color: DarkTheme.normal,
+                          ),
+                        ),
+                        ListTile(
+                          selectedTileColor: Colors.transparent,
+                          contentPadding: EdgeInsets.symmetric(
+                            horizontal: 20,
+                          ),
+                          visualDensity:
+                              VisualDensity(horizontal: 0, vertical: -2),
+                          minLeadingWidth: 0,
+                          title: Text(
+                            'Notification',
+                            style: kThemeData.textTheme.bodyLarge?.copyWith(
+                              color: DarkTheme.normal,
+                              fontSize: 18,
+                            ),
+                          ),
+                          onTap: () async {
+                            // final status = await Permission.notification.op;
+
+                            openAppSettings();
+                          },
+                          leading: SvgPicture.asset(
+                            "assets/images/notification.svg",
+                            height: 26,
+                            // width: 28,
+                            // color: DarkTheme.normal,
+                          ),
+                        ),
+                        ListTile(
+                          selectedTileColor: Colors.transparent,
+                          contentPadding: EdgeInsets.symmetric(
+                            horizontal: 20,
+                          ),
+                          visualDensity:
+                              VisualDensity(horizontal: 0, vertical: -2),
+                          minLeadingWidth: 0,
+                          title: Text(
+                            'Adora Baby',
+                            style: kThemeData.textTheme.bodyLarge?.copyWith(
+                              color: DarkTheme.normal,
+                              fontSize: 18,
+                            ),
+                          ),
+                          onTap: () async {
+                            Uri url = Uri.parse('https://adora.baby/');
+                            if (await canLaunchUrl(url)) {
+                              await launchUrl(url);
+                            } else {
+                              throw 'Could not launch $url';
+                            }
+                          },
+                          leading: SvgPicture.asset(
+                            "assets/images/global.svg",
+                            height: 26,
+                            // width: 28,
+                            // color: DarkTheme.normal,
+                          ),
+                        ),
+                        ListTile(
+                          selectedTileColor: Colors.transparent,
+                          contentPadding: EdgeInsets.symmetric(
+                            horizontal: 20,
+                          ),
+                          visualDensity:
+                              VisualDensity(horizontal: 0, vertical: -2),
+                          minLeadingWidth: 0,
+                          title: Text(
+                            'Facebook',
+                            style: kThemeData.textTheme.bodyLarge?.copyWith(
+                              color: DarkTheme.normal,
+                              fontSize: 18,
+                            ),
+                          ),
+                          onTap: () async {
+                            Uri url = Uri.parse(
+                                'https://www.facebook.com/adorababies');
+                            if (await canLaunchUrl(url)) {
+                              await launchUrl(url);
+                            } else {
+                              throw 'Could not launch $url';
+                            }
+                          },
+                          leading: SvgPicture.asset(
+                            "assets/images/facebook.svg",
+                            height: 26,
+                            // width: 28,
+                            // color: DarkTheme.normal,
+                          ),
+                        ),
+                        ListTile(
+                          selectedTileColor: Colors.transparent,
+                          contentPadding: EdgeInsets.symmetric(
+                            horizontal: 20,
+                          ),
+                          visualDensity:
+                              VisualDensity(horizontal: 0, vertical: -2),
+                          minLeadingWidth: 0,
+                          title: Text(
+                            'Instagram',
+                            style: kThemeData.textTheme.bodyLarge?.copyWith(
+                              color: DarkTheme.normal,
+                              fontSize: 18,
+                            ),
+                          ),
+                          onTap: () async {
+                            Uri url = Uri.parse(
+                                'https://www.instagram.com/_adorababy/');
+                            if (await canLaunchUrl(url)) {
+                              await launchUrl(url);
+                            } else {
+                              throw 'Could not launch $url';
+                            }
+                          },
+                          leading: SvgPicture.asset(
+                            "assets/images/instagram.svg",
+                            height: 26,
+                            // width: 28,
+                            // color: DarkTheme.normal,
+                          ),
+                        ),
+                        ListTile(
+                          selectedTileColor: Colors.transparent,
+                          contentPadding: EdgeInsets.symmetric(
+                            horizontal: 20,
+                          ),
+                          visualDensity:
+                              VisualDensity(horizontal: 0, vertical: -2),
+                          minLeadingWidth: 0,
+                          title: Text(
+                            'Tiktok',
+                            style: kThemeData.textTheme.bodyLarge?.copyWith(
+                              color: DarkTheme.normal,
+                              fontSize: 18,
+                            ),
+                          ),
+                          onTap: () async {
+                            Uri url =
+                                Uri.parse('https://www.tiktok.com/@adora.baby');
+                            if (await canLaunchUrl(url)) {
+                              await launchUrl(url);
+                            } else {
+                              throw 'Could not launch $url';
+                            }
+                          },
+                          leading: SvgPicture.asset(
+                            "assets/images/tiktok.svg",
+                            height: 26,
+                            // width: 28,
+                            // color: DarkTheme.normal,
+                          ),
+                        ),
+                        ListTile(
+                          selectedTileColor: Colors.transparent,
+                          contentPadding: EdgeInsets.symmetric(
+                            horizontal: 20,
+                          ),
+                          visualDensity:
+                              VisualDensity(horizontal: 0, vertical: -4),
+                          minLeadingWidth: 0,
+                          title: Text(
+                            'Contact Us',
+                            style: kThemeData.textTheme.bodyLarge?.copyWith(
+                              color: DarkTheme.normal,
+                              fontSize: 18,
+                            ),
+                          ),
+                          onTap: () async {
+                            Uri url = Uri.parse('tel://+977-9846954208');
+                            if (await canLaunchUrl(url)) {
+                              await launchUrl(url);
+                            } else {
+                              throw 'Could not launch $url';
+                            }
+                          },
+                          leading: SvgPicture.asset(
+                            "assets/images/profile_call.svg",
+                            height: 26,
+                            // width: 28,
+                            // color: DarkTheme.normal,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                  onTap: () async {
-                    try {
-                      await storage.delete(
-                        Constants.ACCESS_TOKEN,
-                      );
-                      await storage.delete(
-                        Constants.LOGGED_IN_STATUS,
-                      );
-                      await storage.delete(
-                        Constants.REFRESH_TOKEN,
-                      );
-                      Get.offAllNamed(Routes.PHONE);
-                    } catch (e) {
-                      Get.offAllNamed(Routes.PHONE);
-                    }
-                  },
-                  trailing: Icon(
-                    Icons.logout,
-                    color: DarkTheme.normal,
+                  Divider(
+                    color: DarkTheme.darkLightActive,
                   ),
-                ),
-              ],
+                  ListTile(
+                    title: Text(
+                      'Logout',
+                      style: kThemeData.textTheme.bodyLarge
+                          ?.copyWith(color: DarkTheme.normal, fontSize: 18),
+                    ),
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 24,
+                    ),
+                    minLeadingWidth: 0,
+                    onTap: () async {
+                      try {
+                        await storage.delete(
+                          Constants.ACCESS_TOKEN,
+                        );
+                        await storage.delete(
+                          Constants.LOGGED_IN_STATUS,
+                        );
+                        await storage.delete(
+                          Constants.REFRESH_TOKEN,
+                        );
+                        Get.offAllNamed(Routes.PHONE);
+                      } catch (e) {
+                        Get.offAllNamed(Routes.PHONE);
+                      }
+                    },
+                    leading: SvgPicture.asset(
+                      "assets/images/logout.svg",
+                      height: 28,
+                      color: DarkTheme.normal,
+                    ),
+                  ),
+                  Divider(
+                    color: DarkTheme.darkLightActive,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  GestureDetector(
+                    onTap: () async {
+                      Uri url = Uri.parse('https://www.google.com/search?q=adora+baby+terms+and+condition');
+                      if (await canLaunchUrl(url)) {
+                      await launchUrl(url);
+                      } else {
+                      throw 'Could not launch $url';
+                      }
+                    },
+                    child: Text(
+                      'Terms of Service',
+                      textAlign: TextAlign.center,
+                      style: kThemeData.textTheme.bodyMedium?.copyWith(
+                        color: DarkTheme.normal,
+                        fontSize: 16,
+                        // fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
